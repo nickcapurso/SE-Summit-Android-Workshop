e261a8545d526e040cfe0429d4c55f7796944f6a
Nick Capurso <capurso.nick@gmail.com>
2018-05-13 15:11:46 -0400

    Simple working project

--- app/src/main/java/com/summit/summitproject/LoginActivity.java
+++ app/src/main/java/com/summit/summitproject/LoginActivity.java
@@ -1,350 +1,186 @@
 package com.summit.summitproject;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
-
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.AsyncTask;
-
-import android.os.Build;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.text.TextUtils;
-import android.view.KeyEvent;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.summit.summitproject.login.LoginListener;
+import com.summit.summitproject.login.LoginManager;
+import com.summit.summitproject.model.Transaction;
 
 import java.util.ArrayList;
-import java.util.List;
-
-import static android.Manifest.permission.READ_CONTACTS;
 
 /**
- * A login screen that offers login via email/password.
+ * The first screen of our app. Takes in a username and password and interacts with the
+ * {@link LoginManager} to retrieve user details. Also allows the user to check "Remember Me"
+ * to locally store and recall credentials.
  */
-public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
+public class LoginActivity extends AppCompatActivity {
 
     /**
-     * Id to identity READ_CONTACTS permission request.
+     * The key under which the <b>username</b> will be stored in {@link SharedPreferences}.
      */
-    private static final int REQUEST_READ_CONTACTS = 0;
+    private static final String PREF_USERNAME = "USERNAME";
 
     /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
+     * The key under which the <b>password</b> will be stored in {@link SharedPreferences}.
      */
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
-    };
+    private static final String PREF_PASSWORD = "PASSWORD";
+
     /**
-     * Keep track of the login task to ensure we can cancel it if requested.
+     * Used to persist user credentials if "Remember Me" is checked.
      */
-    private UserLoginTask mAuthTask = null;
+    private SharedPreferences sharedPreferences;
 
-    // UI references.
-    private AutoCompleteTextView mEmailView;
-    private EditText mPasswordView;
-    private View mProgressView;
-    private View mLoginFormView;
+    // UI Widgets
 
+    private EditText username;
+
+    private EditText password;
+
+    private Button signIn;
+
+    private CheckBox rememberMe;
+
+    private ProgressBar progress;
+
+    /**
+     * Called the first time an Activity is created, but before any UI is shown to the user.
+     * Prepares the layout and assigns UI widget variables.
+     */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        // Set up the login form.
-        mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
-        populateAutoComplete();
 
-        mPasswordView = (EditText) findViewById(R.id.password);
-        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        signIn = findViewById(R.id.sign_in);
+        rememberMe = findViewById(R.id.remember_me);
+        progress = findViewById(R.id.progress);
+
+        setupWidgets();
+
+        // If user credentials were previously stored, pre-fill the username / password
+        sharedPreferences = getSharedPreferences("prefs", MODE_PRIVATE);
+        rememberMe.setChecked(sharedPreferences.contains(PREF_USERNAME));
+        username.setText(sharedPreferences.getString(PREF_USERNAME, ""));
+        password.setText(sharedPreferences.getString(PREF_PASSWORD, ""));
+    }
+
+    /**
+     * Set up listeners for various user interactions.
+     */
+    private void setupWidgets() {
+        username.addTextChangedListener(textWatcher);
+        password.addTextChangedListener(textWatcher);
+
+        signIn.setOnClickListener(new OnClickListener() {
             @Override
-            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
-                if (id == EditorInfo.IME_ACTION_DONE || id == EditorInfo.IME_NULL) {
-                    attemptLogin();
-                    return true;
+            public void onClick(View v) {
+                // Store user credentials in private storage if "Remember Me" was checked
+                if (rememberMe.isChecked()) {
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putString(PREF_USERNAME, username.getText().toString());
+                    editor.putString(PREF_PASSWORD, password.getText().toString());
+                    editor.apply();
                 }
-                return false;
-            }
-        });
 
-        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
-        mEmailSignInButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                attemptLogin();
-            }
-        });
+                // Don't allow user input while logging in & show the progress bar
+                setAllEnabled(false);
+                progress.setVisibility(View.VISIBLE);
 
-        mLoginFormView = findViewById(R.id.login_form);
-        mProgressView = findViewById(R.id.login_progress);
-    }
+                // Instantiate the login manager, passing the username, password, and result listener
+                LoginManager loginManager = new LoginManager(
+                        username.getText().toString(),
+                        password.getText().toString(),
+                        new LoginListener() {
+                            @Override
+                            public void onLoginSuccess(String name, String cardNum, ArrayList<Transaction> transactions) {
+                                // Allow user input (eg. if the user returns to this screen) and
+                                // hide the progress bar again
+                                setAllEnabled(true);
+                                progress.setVisibility(View.INVISIBLE);
 
-    private void populateAutoComplete() {
-        if (!mayRequestContacts()) {
-            return;
-        }
+                                // Start the SummaryActivity and also pass the user's name,
+                                // card number, and list of transactions in the launch intent.
+                                Intent intent = new Intent(LoginActivity.this, SummaryActivity.class);
+                                intent.putExtra(SummaryActivity.KEY_NAME, name);
+                                intent.putExtra(SummaryActivity.KEY_CARD_NUM, cardNum);
+                                intent.putExtra(SummaryActivity.KEY_TRANSACTIONS, transactions);
+                                startActivity(intent);
+                            }
 
-        getLoaderManager().initLoader(0, null, this);
-    }
+                            @Override
+                            public void onLoginError(Exception exception) {
+                                // Allow user input (eg. if the user returns to this screen) and
+                                // hide the progress bar again
+                                setAllEnabled(true);
+                                progress.setVisibility(View.INVISIBLE);
 
-    private boolean mayRequestContacts() {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
-            return true;
-        }
-        if (checkSelfPermission(READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
-            return true;
-        }
-        if (shouldShowRequestPermissionRationale(READ_CONTACTS)) {
-            Snackbar.make(mEmailView, R.string.permission_rationale, Snackbar.LENGTH_INDEFINITE)
-                    .setAction(android.R.string.ok, new View.OnClickListener() {
-                        @Override
-                        @TargetApi(Build.VERSION_CODES.M)
-                        public void onClick(View v) {
-                            requestPermissions(new String[]{READ_CONTACTS}, REQUEST_READ_CONTACTS);
+                                Toast.makeText(LoginActivity.this, "Failed to login", Toast.LENGTH_LONG).show();
+                            }
                         }
-                    });
-        } else {
-            requestPermissions(new String[]{READ_CONTACTS}, REQUEST_READ_CONTACTS);
-        }
-        return false;
-    }
+                );
 
-    /**
-     * Callback received when a permissions request has been completed.
-     */
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
-        if (requestCode == REQUEST_READ_CONTACTS) {
-            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                populateAutoComplete();
+                // Kick off the login network call
+                loginManager.execute();
             }
-        }
-    }
+        });
 
-
-    /**
-     * Attempts to sign in or register the account specified by the login form.
-     * If there are form errors (invalid email, missing fields, etc.), the
-     * errors are presented and no actual login attempt is made.
-     */
-    private void attemptLogin() {
-        if (mAuthTask != null) {
-            return;
-        }
-
-        // Reset errors.
-        mEmailView.setError(null);
-        mPasswordView.setError(null);
-
-        // Store values at the time of the login attempt.
-        String email = mEmailView.getText().toString();
-        String password = mPasswordView.getText().toString();
-
-        boolean cancel = false;
-        View focusView = null;
-
-        // Check for a valid password, if the user entered one.
-        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
-            mPasswordView.setError(getString(R.string.error_invalid_password));
-            focusView = mPasswordView;
-            cancel = true;
-        }
-
-        // Check for a valid email address.
-        if (TextUtils.isEmpty(email)) {
-            mEmailView.setError(getString(R.string.error_field_required));
-            focusView = mEmailView;
-            cancel = true;
-        } else if (!isEmailValid(email)) {
-            mEmailView.setError(getString(R.string.error_invalid_email));
-            focusView = mEmailView;
-            cancel = true;
-        }
-
-        if (cancel) {
-            // There was an error; don't attempt login and focus the first
-            // form field with an error.
-            focusView.requestFocus();
-        } else {
-            // Show a progress spinner, and kick off a background task to
-            // perform the user login attempt.
-            showProgress(true);
-            mAuthTask = new UserLoginTask(email, password);
-            mAuthTask.execute((Void) null);
-        }
-    }
-
-    private boolean isEmailValid(String email) {
-        //TODO: Replace this with your own logic
-        return email.contains("@");
-    }
-
-    private boolean isPasswordValid(String password) {
-        //TODO: Replace this with your own logic
-        return password.length() > 4;
-    }
-
-    /**
-     * Shows the progress UI and hides the login form.
-     */
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
-    private void showProgress(final boolean show) {
-        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
-        // for very easy animations. If available, use these APIs to fade-in
-        // the progress spinner.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
-
-            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-            mLoginFormView.animate().setDuration(shortAnimTime).alpha(
-                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-                }
-            });
-
-            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-            mProgressView.animate().setDuration(shortAnimTime).alpha(
-                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-                }
-            });
-        } else {
-            // The ViewPropertyAnimator APIs are not available, so simply show
-            // and hide the relevant UI components.
-            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-        }
-    }
-
-    @Override
-    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
-        return new CursorLoader(this,
-                // Retrieve data rows for the device user's 'profile' contact.
-                Uri.withAppendedPath(ContactsContract.Profile.CONTENT_URI,
-                        ContactsContract.Contacts.Data.CONTENT_DIRECTORY), ProfileQuery.PROJECTION,
-
-                // Select only email addresses.
-                ContactsContract.Contacts.Data.MIMETYPE +
-                        " = ?", new String[]{ContactsContract.CommonDataKinds.Email
-                .CONTENT_ITEM_TYPE},
-
-                // Show primary email addresses first. Note that there won't be
-                // a primary email address if the user hasn't specified one.
-                ContactsContract.Contacts.Data.IS_PRIMARY + " DESC");
-    }
-
-    @Override
-    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
-        List<String> emails = new ArrayList<>();
-        cursor.moveToFirst();
-        while (!cursor.isAfterLast()) {
-            emails.add(cursor.getString(ProfileQuery.ADDRESS));
-            cursor.moveToNext();
-        }
-
-        addEmailsToAutoComplete(emails);
-    }
-
-    @Override
-    public void onLoaderReset(Loader<Cursor> cursorLoader) {
-
-    }
-
-    private void addEmailsToAutoComplete(List<String> emailAddressCollection) {
-        //Create adapter to tell the AutoCompleteTextView what to show in its dropdown list.
-        ArrayAdapter<String> adapter =
-                new ArrayAdapter<>(LoginActivity.this,
-                        android.R.layout.simple_dropdown_item_1line, emailAddressCollection);
-
-        mEmailView.setAdapter(adapter);
-    }
-
-
-    private interface ProfileQuery {
-        String[] PROJECTION = {
-                ContactsContract.CommonDataKinds.Email.ADDRESS,
-                ContactsContract.CommonDataKinds.Email.IS_PRIMARY,
-        };
-
-        int ADDRESS = 0;
-        int IS_PRIMARY = 1;
-    }
-
-    /**
-     * Represents an asynchronous login/registration task used to authenticate
-     * the user.
-     */
-    public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
-
-        private final String mEmail;
-        private final String mPassword;
-
-        UserLoginTask(String email, String password) {
-            mEmail = email;
-            mPassword = password;
-        }
-
-        @Override
-        protected Boolean doInBackground(Void... params) {
-            // TODO: attempt authentication against a network service.
-
-            try {
-                // Simulate network access.
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                return false;
-            }
-
-            for (String credential : DUMMY_CREDENTIALS) {
-                String[] pieces = credential.split(":");
-                if (pieces[0].equals(mEmail)) {
-                    // Account exists, return true if the password matches.
-                    return pieces[1].equals(mPassword);
+        rememberMe.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // If "Remember Me" was previously checked and the user no longer wants their
+                // credentials remembered, clear the credentials from storage.
+                if (!isChecked) {
+                    username.setText("");
+                    password.setText("");
+                    sharedPreferences.edit().clear().apply();
                 }
             }
+        });
+    }
 
-            // TODO: register the new account here.
-            return true;
-        }
+    /**
+     * Used with {@link android.widget.TextView#addTextChangedListener(TextWatcher)} to get callbacks
+     * when the user interacts with the text input widgets.
+     */
+    private TextWatcher textWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
 
         @Override
-        protected void onPostExecute(final Boolean success) {
-            mAuthTask = null;
-            showProgress(false);
-
-            if (success) {
-                finish();
-            } else {
-                mPasswordView.setError(getString(R.string.error_incorrect_password));
-                mPasswordView.requestFocus();
-            }
-        }
+        public void onTextChanged(CharSequence s, int start, int before, int count) {}
 
         @Override
-        protected void onCancelled() {
-            mAuthTask = null;
-            showProgress(false);
+        public void afterTextChanged(Editable s) {
+            // Enable the "Sign In" button if something has been inputted in both the username
+            // and password.
+            String usernameText = username.getText().toString().trim();
+            String passwordText = password.getText().toString().trim();
+            signIn.setEnabled(usernameText.length() > 0 && passwordText.length() > 0);
         }
+    };
+
+    /**
+     * Enables or disables user interactions with the text input widgets & sign in button.
+     */
+    private void setAllEnabled(boolean enabled) {
+        username.setEnabled(enabled);
+        password.setEnabled(enabled);
+        signIn.setEnabled(enabled);
     }
 }
 
---
+++ app/src/main/java/com/summit/summitproject/model/Transaction.java
@@ -0,0 +1,27 @@
+package com.summit.summitproject.model;
+
+import java.io.Serializable;
+
+/**
+ * Represents a simple credit card transaction -- containing the merchant and the amount.
+ */
+public class Transaction implements Serializable {
+
+    private final String merchant;
+
+    private final String amount;
+
+    public Transaction(String merchant, String amount) {
+        this.merchant = merchant;
+        this.amount = amount;
+    }
+
+    public String getMerchant() {
+        return merchant;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+}
